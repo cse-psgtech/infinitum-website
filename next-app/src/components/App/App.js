3f0d2f99e5b4729a2c1eb5bc68fa32e4189923dd
Adhish-Krishna <adhishthesak@gmail.com>
2026-01-07 11:22:21 +0530

    removed footer on all other pages other than the /route

--- next-app/src/components/App/App.js
+++ next-app/src/components/App/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
+import { usePathname } from 'next/navigation';
 
 import { AppContent } from '../AppContent';
 import { Header } from '../Header';
@@ -11,7 +12,11 @@ import { useShutter } from '@/context/ShutterContext';
 // Wrapper component to use hooks
 function AppWithShutter({ classes, className, children, ...etc }) {
   const { shutterState } = useShutter();
+  const pathname = usePathname();
   const isShuttering = shutterState === 'closing' || shutterState === 'closed' || shutterState === 'opening';
+
+  // Only show footer on home route
+  const showFooter = pathname === '/';
 
   return (
     <div className={cx(classes.root, className)} {...etc}>
@@ -28,7 +33,7 @@ function AppWithShutter({ classes, className, children, ...etc }) {
         <AppContent>
           {children}
         </AppContent>
-        <Footer />
+        {showFooter && <Footer />}
       </div>
     </div>
   );
